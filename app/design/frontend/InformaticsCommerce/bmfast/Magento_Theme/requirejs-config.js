e3e97c3ba449c8e46cf4abbe398221f607948c9e
Debian <bitnami@ip-172-26-6-82.ap-south-1.compute.internal>
2021-04-05 14:46:08 +0000

    sync files 5-april

---
+++ app/design/frontend/InformaticsCommerce/bmfast/Magento_Theme/requirejs-config.js
@@ -0,0 +1,13 @@
+var config = {
+	"map": {
+		"*": {
+			"theme/owlcarousel": "Magento_Theme/js/owl.carousel.min"
+		}
+	},
+	"paths": {           
+		"theme/owlcarousel": "Magento_Theme/js/owl.carousel.min"
+	},   
+    "shim": {
+		"Magento_Theme/js/owl.carousel.min": ["jquery"]
+	}
+};
