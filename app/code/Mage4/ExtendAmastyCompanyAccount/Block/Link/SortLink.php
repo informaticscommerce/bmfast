b1d9a9bce977a166e5d989a057cb48be310be392
Ashok <ashok@ashok>
2021-08-26 19:25:14 +0500

    B2B B2C customer account tab

--- app/code/Mage4/ExtendAmastyCompanyAccount/Block/Link/SortLink.php
+++ app/code/Mage4/ExtendAmastyCompanyAccount/Block/Link/SortLink.php
@@ -26,46 +26,57 @@ class SortLink extends \Amasty\CompanyAccount\Block\Link\AbstractLink
             return parent::_toHtml();
         }
 
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $customerSession = $objectManager->get('Magento\Customer\Model\Session');
+        $customerID = $customerSession->getCustomer()->getId();//get id of customer
+
+        $model = $objectManager->create('Amasty\CompanyAccount\Model\Company');
+        $collection = $model->getCollection();
+        $collection->addFieldToFilter('super_user_id', $customerID);
+
         $highlight = '';
+        $html = '';
 
         if ($this->getIsHighlighted()) {
             $highlight = ' current';
         }
 
-        if ($this->isCurrent()) {
-            /*
-             * custom work,
-             * append label in class
-             * */
-            $labelString = str_replace('/', '-', strtolower($this->getLabel()));
-            $html = '<li class="nav item current '.str_replace(' ', '-', $labelString).'">';
-            $html .= '<strong>'
-                . $this->escapeHtml(__($this->getLabel()))
-                . '</strong>';
-            $html .= '</li>';
-        } else {
-            /*
-             * custom work,
-             * append label in class
-             * */
-            $labelString = str_replace('/', '-', strtolower($this->getLabel()));
-            $html = '<li class="nav item ' .str_replace(' ', '-', $labelString). $highlight . '"><a href="' . $this->escapeHtml($this->getHref()) . '"';
-            $html .= $this->getTitle()
-                ? ' title="' . $this->escapeHtml(__($this->getTitle())) . '"'
-                : '';
-            $html .= $this->getAttributesHtml() . '>';
+        if (count($collection) != 0) {
+            if ($this->isCurrent()) {
+                /*
+                 * custom work,
+                 * append label in class
+                 * */
+                $labelString = str_replace('/', '-', strtolower($this->getLabel()));
+                $html = '<li class="nav item current ' . str_replace(' ', '-', $labelString) . '">';
+                $html .= '<strong>'
+                    . $this->escapeHtml(__($this->getLabel()))
+                    . '</strong>';
+                $html .= '</li>';
+            } else {
+                /*
+                 * custom work,
+                 * append label in class
+                 * */
+                $labelString = str_replace('/', '-', strtolower($this->getLabel()));
+                $html = '<li class="nav item ' . str_replace(' ', '-', $labelString) . $highlight . '"><a href="' . $this->escapeHtml($this->getHref()) . '"';
+                $html .= $this->getTitle()
+                    ? ' title="' . $this->escapeHtml(__($this->getTitle())) . '"'
+                    : '';
+                $html .= $this->getAttributesHtml() . '>';
 
-            if ($this->getIsHighlighted()) {
-                $html .= '<strong>';
+                if ($this->getIsHighlighted()) {
+                    $html .= '<strong>';
+                }
+
+                $html .= $this->escapeHtml(__($this->getLabel()));
+
+                if ($this->getIsHighlighted()) {
+                    $html .= '</strong>';
+                }
+
+                $html .= '</a></li>';
             }
-
-            $html .= $this->escapeHtml(__($this->getLabel()));
-
-            if ($this->getIsHighlighted()) {
-                $html .= '</strong>';
-            }
-
-            $html .= '</a></li>';
         }
 
         return $html;
--- app/code/Mage4/ExtendMagentoCustomer/Block/Account/SortLink.php
+++ app/code/Mage4/ExtendMagentoCustomer/Block/Account/SortLink.php
@@ -11,6 +11,7 @@ namespace Mage4\ExtendMagentoCustomer\Block\Account;
  */
 class SortLink extends \Magento\Customer\Block\Account\SortLink
 {
+
     /**
      * Render block HTML
      *
@@ -22,46 +23,61 @@ class SortLink extends \Magento\Customer\Block\Account\SortLink
             return parent::_toHtml();
         }
 
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $customerSession = $objectManager->get('Magento\Customer\Model\Session');
+        $customerID = $customerSession->getCustomer()->getId();//get id of customer
+
+        $model = $objectManager->create('Amasty\CompanyAccount\Model\Company');
+        $collection = $model->getCollection();
+        $collection->addFieldToFilter('super_user_id', $customerID);
+
+
         $highlight = '';
+        $html = '';
 
         if ($this->getIsHighlighted()) {
             $highlight = ' current';
         }
 
-        if ($this->isCurrent()) {
-            /*
-             * custom work,
-             * append label in class
-             * */
-            $labelString = str_replace('/', '-', strtolower($this->getLabel()));
-            $html = '<li class="nav item current '.str_replace(' ', '-', $labelString).'">';
-            $html .= '<strong>'
-                . $this->escapeHtml(__($this->getLabel()))
-                . '</strong>';
-            $html .= '</li>';
-        } else {
-            /*
-             * custom work,
-             * append label in class
-             * */
-            $labelString = str_replace('/', '-', strtolower($this->getLabel()));
-            $html = '<li class="nav item ' .str_replace(' ', '-', $labelString). $highlight . '"><a href="' . $this->escapeHtml($this->getHref()) . '"';
-            $html .= $this->getTitle()
-                ? ' title="' . $this->escapeHtml(__($this->getTitle())) . '"'
-                : '';
-            $html .= $this->getAttributesHtml() . '>';
+        // hide Order tab from customer account for company account
+        if ( (count($collection) > 0 && strpos(strtolower($this->getLabel()), 'orders') === false) || (count($collection) == 0 ) ) {
+            if ($this->isCurrent()) {
 
-            if ($this->getIsHighlighted()) {
-                $html .= '<strong>';
+                /*
+                 * custom work,
+                 * append label in class
+                 * */
+                $labelString = str_replace('/', '-', strtolower($this->getLabel()));
+                $html = '<li class="nav item current ' . str_replace(' ', '-', $labelString) . '">';
+                $html .= '<strong>'
+                    . $this->escapeHtml(__($this->getLabel()))
+                    . '</strong>';
+                $html .= '</li>';
+            } else {
+                /*
+                 * custom work,
+                 * append label in class
+                 * */
+                $labelString = str_replace('/', '-', strtolower($this->getLabel()));
+                $html = '<li class="nav item ' . str_replace(' ', '-', $labelString) . $highlight . '"><a href="' . $this->escapeHtml($this->getHref()) . '"';
+                $html .= $this->getTitle()
+                    ? ' title="' . $this->escapeHtml(__($this->getTitle())) . '"'
+                    : '';
+                $html .= $this->getAttributesHtml() . '>';
+
+                if ($this->getIsHighlighted()) {
+                    $html .= '<strong>';
+                }
+
+                $html .= $this->escapeHtml(__($this->getLabel()));
+
+                if ($this->getIsHighlighted()) {
+                    $html .= '</strong>';
+                }
+
+                $html .= '</a></li>';
+
             }
-
-            $html .= $this->escapeHtml(__($this->getLabel()));
-
-            if ($this->getIsHighlighted()) {
-                $html .= '</strong>';
-            }
-
-            $html .= '</a></li>';
         }
 
         return $html;
